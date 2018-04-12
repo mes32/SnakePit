f9be30ee77ca3fd001d61354f0f1c7f2cacb65af
mes32 <stockman.mike@gmail.com>
2018-04-12 15:27:01 -0400

    refactor: Initialize the pygame screen object in Application using constants stored in GameLevelView.

--- snakepit/application.py
+++ snakepit/application.py
@@ -1,9 +1,10 @@
 import pygame
 
 import game_loop
+import game_level_view
 
 title = "Snake Pit"
-screen_size = width, height = 576, 596
+# screen_size = width, height = 576, 596
 
 class Application():
     """
@@ -13,6 +14,14 @@ class Application():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(title)
+
+        fov = game_level_view.GameLevelView.FIELD_OF_VIEW
+        cell_dim = game_level_view.GameLevelView.GRID_CELL
+        display_dim = game_level_view.GameLevelView.DISPLAY_BAR_CELL
+        width = fov.width * cell_dim.width
+        height = fov.height * cell_dim.height + display_dim.height
+        screen_size = (width, height)
+
         self.screen = pygame.display.set_mode(screen_size)
 
     def run(self):
